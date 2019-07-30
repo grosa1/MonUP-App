239b785aea8fe1e7dc952680d026978aa17605e8
ZappaBoy <federico-zappone@hotmail.it>
2019-07-30 14:54:22 +0200

    Refactorized Code

--- app/src/main/java/giovanni/tradingtoolkit/home_widget/CoinListWidget.java
+++ app/src/main/java/giovanni/tradingtoolkit/home_widget/CoinListWidget.java
@@ -29,42 +29,14 @@ import giovanni.tradingtoolkit.marketprices.CoinsListAdapter;
  */
 public class CoinListWidget extends AppWidgetProvider {
 
-    private ArrayList<String> coinsToObserve;
-    private CoinsListAdapter.CoinItemListener itemListener;
-    private ListView listView;
-    private List<Coin> coins;
-    private ArrayList<Coin> coinsToShow;
-
-
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
         //  CharSequence widgetText = CoinListWidgetConfigureActivity.loadTitlePref(context, appWidgetId);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.personalizable_coin_list_widget);
-        //views.setTextViewText(R.id.appwidget_text, widgetText);
-        //
         Intent intent = new Intent(context, WidgetService.class);
-        //
-        // intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,appWidgetIds[i]);
-        //intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
-
-//        adapter = new CoinsListAdapter(context, coinsToShow, coinSym -> {
-//            if (!(currency.equals("BTC") && coinSym.equals("BTC"))) {
-//                this.showChart(coinSym, currency);
-//            }
-//        });
-        //Intent intent = new Intent(context, CoinsListAdapter.class);
-
-
         views.setRemoteAdapter(R.id.list, intent);
-
-
-//        CoinsListAdapter coinsListAdapter = new CoinsListAdapter(context, coins, itemListener);
-//        coinsListAdapter.filterList(filteredList);
-//        recyclerView.setAdapter(coinsListAdapter);
-//        recyclerView.setLayoutManager(new LinearLayoutManager(context));
-        //ListView coinsList =
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -76,22 +48,11 @@ public class CoinListWidget extends AppWidgetProvider {
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
-
-        //loadSerialCoins(context);
-        //restorePreferences(context);
-        //itemListener = coinSymbol -> {
-//            Log.e("ITEM SELECTED", coinSymbol);
-//
-//            // TODO: Show chart
-//        };
     }
 
     @Override
     public void onDeleted(Context context, int[] appWidgetIds) {
         // When the user deletes the widget, delete the preference associated with it.
-        // for (int appWidgetId : appWidgetIds) {
-        //    CoinListWidgetConfigureActivity.deleteTitlePref(context, appWidgetId);
-        //}
     }
 
     @Override
@@ -103,72 +64,5 @@ public class CoinListWidget extends AppWidgetProvider {
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
     }
-
-//    private void restorePreferences(Context context) {
-//        coinsToObserve = new ArrayList<>();
-//        String storedPreferences = SharedPrefs.restoreString(context, SharedPrefs.KEY_COINS_WIDGET);
-//
-//        if (!storedPreferences.isEmpty()) {
-//
-//            String[] parts = storedPreferences.split(" ,");
-//
-//            coinsToObserve.addAll(Arrays.asList(parts));
-//            loadSerialCoins(context);
-//            getCoinsToShow();
-//            makeToast(context, coinsToObserve.toString());
-//            Log.e("PREFERENCES-Restored", coinsToObserve.toString());
-//        }
-//    }
-
-    private void makeToast(Context context, String text_content) {
-        Log.e("Toast", text_content);
-        Toast.makeText(context, text_content, Toast.LENGTH_SHORT).show();
-    }
-
-//    private Coin getCoinBySymbol(String coinSymbol) {
-//
-//        for (Coin coin : coins) {
-//
-//            Log.e("COIN-SYMBOL", coin.getSymbol());
-//            Log.e("COIN-SYMBOL", coin.getId());
-//            Log.e("COIN-SYMBOL-STORED", coinSymbol);
-//            if (coin.getSymbol().contains(coinSymbol)) {
-//                Log.e("COIN-FOUND", coinSymbol);
-//
-//                return coin;
-//            }
-//        }
-//        return null;
-//    }
-//
-//    private void getCoinsToShow() {
-//        ArrayList<Coin> coinsToShow = new ArrayList<>();
-//
-//        Log.e("COINS", coins.toString());
-//        List<String> coinsToFind = Arrays.asList(coinsToObserve.toString().split(","));
-//        Log.e("COINTOFIND", coinsToFind.toString());
-//
-//        for (String coin : coinsToFind) {
-//            coin = coin.replace("[", "").replace("]", "");
-//
-//            if (!coin.isEmpty()) {
-//                Log.e("COINTOFIND-SYM", coin);
-//
-//                Coin coinToAdd = getCoinBySymbol(coin);
-//                assert coinToAdd != null;
-//                Log.e("COINTOADD", coinToAdd.toString());
-//                coinsToShow.add(coinToAdd);
-//
-//            }
-//        }
-//    }
-//
-//    private void loadSerialCoins(Context context) {
-//        String serialCoins = SharedPrefs.restoreString(context, SharedPrefs.KEY_COINS_CACHE);
-//        Type listType = new TypeToken<ArrayList<Coin>>() {
-//        }.getType();
-//        coins = (new Gson()).fromJson(serialCoins, listType);
-//        Log.e("LOAD_COINS", coins.toString());
-//    }
 }
 
--- app/src/main/java/giovanni/tradingtoolkit/home_widget/WidgetRemoteViewsFactory.java
+++ app/src/main/java/giovanni/tradingtoolkit/home_widget/WidgetRemoteViewsFactory.java
@@ -3,7 +3,6 @@ package giovanni.tradingtoolkit.home_widget;
 import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.widget.LinearLayoutManager;
 import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
@@ -21,21 +20,15 @@ import giovanni.tradingtoolkit.data.model.Coin;
 import giovanni.tradingtoolkit.main.SharedPrefs;
 import giovanni.tradingtoolkit.marketprices.CoinsListAdapter;
 
-public class WidgetRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory
-{
-    private Context context = null;
+public class WidgetRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
+    private Context context;
     private int appWidgetId;
     private ArrayList<Coin> coinsToShow;
     private ArrayList<String> coinsToObserve;
     private List<Coin> coins;
     private CoinsListAdapter.CoinItemListener itemListener;
 
-
-    private List<String> widgetList = new ArrayList<String>();
-    //private DBHelper dbhelper;
-
-    public WidgetRemoteViewsFactory(Context context, Intent intent)
-    {
+    public WidgetRemoteViewsFactory(Context context, Intent intent) {
         this.context = context;
         appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
                 AppWidgetManager.INVALID_APPWIDGET_ID);
@@ -52,120 +45,94 @@ public class WidgetRemoteViewsFactory implements RemoteViewsService.RemoteViewsF
 
         CoinsListAdapter coinsListAdapter = new CoinsListAdapter(context, coinsToShow, itemListener);
         coinsListAdapter.filterList(coinsToShow);
-
-        //Intent intent = new Intent(context, coinsListAdapter.getClass());
-        //remoteView.setRemoteAdapter(R.id.list, intent);
-
-        //dbhelper = new DBHelper(this.context);
     }
 
-    private void updateWidgetListView()
-    {
-        //String[] coins = {"ETH","BTC","BNB","ZEC"}; // TODO: da eliminare
-        //List<String> convertedToList = new ArrayList<String>(Arrays.asList(coins));
-        //this.widgetList = convertedToList;
+    private void updateWidgetListView() {
+        //TODO: Refresh dati
     }
 
     @Override
-    public int getCount()
-    {
+    public int getCount() {
         return coinsToShow.size();
     }
 
     @Override
-    public long getItemId(int position)
-    {
+    public long getItemId(int position) {
         return position;
     }
 
     @Override
-    public RemoteViews getLoadingView()
-    {
+    public RemoteViews getLoadingView() {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public RemoteViews getViewAt(int position)
-    {
+    public RemoteViews getViewAt(int position) {
         Log.d("WidgetCreatingView", "WidgetCreatingView");
         RemoteViews remoteView = new RemoteViews(context.getPackageName(),
                 R.layout.coin_list_item);
-
-        Log.d("Loading", coinsToShow.get(position).toString());
-
-        itemListener = coinSymbol -> {
-            Log.e("ITEM SELECTED", coinSymbol);
-
-        };
 
         CoinsListAdapter coinsListAdapter = new CoinsListAdapter(context, coinsToShow, itemListener);
         coinsListAdapter.filterList(coinsToShow);
 
         Intent intent = new Intent(context, coinsListAdapter.getClass());
         Coin coinItem = coinsToShow.get(position);
-        remoteView.setTextViewText(R.id.coin_name, coinItem.getName());
-        remoteView.setTextViewText(R.id.list_position, "TODO");
-        // remoteView.setTextViewText(R.id.icon, "TODO");
-        remoteView.setTextViewText(R.id.price, coinItem.getPriceBtc());
-        remoteView.setTextViewText(R.id.percentage_variation_1h, Float.toString((float)coinItem.getPercentChange1h()));
-        remoteView.setTextViewText(R.id.percentage_variation_1d, Float.toString((float)coinItem.getPercentChange24h()));
-        remoteView.setTextViewText(R.id.percentage_variation_1w, Float.toString((float)coinItem.getPercentChange7d()));
 
-
+        remoteView = setCoinInfo(remoteView, coinItem);
         remoteView.setRemoteAdapter(R.id.list, intent);
 
-        //remoteView.setTextViewText(R.id.list, coinsToShow.get(position).getSymbol());
-        Log.d("CHECK", coinsToShow.get(position).getSymbol());
         return remoteView;
     }
 
+    private RemoteViews setCoinInfo(RemoteViews rv, Coin coinItem) {
+
+        rv.setTextViewText(R.id.coin_name, coinItem.getName());
+        rv.setTextViewText(R.id.list_position, "TODO");
+        // rv.setTextViewText(R.id.icon, "TODO");
+        rv.setTextViewText(R.id.price, coinItem.getPriceBtc());
+        rv.setTextViewText(R.id.percentage_variation_1h, Float.toString((float) coinItem.getPercentChange1h()));
+        rv.setTextViewText(R.id.percentage_variation_1d, Float.toString((float) coinItem.getPercentChange24h()));
+        rv.setTextViewText(R.id.percentage_variation_1w, Float.toString((float) coinItem.getPercentChange7d()));
+
+        return rv;
+    }
+
     @Override
-    public int getViewTypeCount()
-    {
+    public int getViewTypeCount() {
         // TODO Auto-generated method stub
         return 1;
     }
 
     @Override
-    public boolean hasStableIds()
-    {
+    public boolean hasStableIds() {
         // TODO Auto-generated method stub
         return false;
     }
 
     @Override
-    public void onCreate()
-    {
+    public void onCreate() {
         // TODO Auto-generated method stub
         updateWidgetListView();
     }
 
     @Override
-    public void onDataSetChanged()
-    {
+    public void onDataSetChanged() {
         // TODO Auto-generated method stub
         updateWidgetListView();
     }
 
     @Override
-    public void onDestroy()
-    {
+    public void onDestroy() {
         // TODO Auto-generated method stub
         coinsToShow.clear();
-      //  dbhelper.close();
     }
 
     private Coin getCoinBySymbol(String coinSymbol) {
 
         for (Coin coin : coins) {
-
-            Log.e("COIN-SYMBOL", coin.getSymbol());
-            Log.e("COIN-SYMBOL", coin.getId());
-            Log.e("COIN-SYMBOL-STORED", coinSymbol);
             if (coin.getSymbol().contains(coinSymbol)) {
                 Log.e("COIN-FOUND", coinSymbol);
-
                 return coin;
             }
         }
@@ -174,22 +141,15 @@ public class WidgetRemoteViewsFactory implements RemoteViewsService.RemoteViewsF
 
     private void getCoinsToShow() {
         coinsToShow = new ArrayList<>();
-
-        Log.e("COINS", coins.toString());
-        List<String> coinsToFind = Arrays.asList(coinsToObserve.toString().split(","));
-        Log.e("COINTOFIND", coinsToFind.toString());
+        String[] coinsToFind = coinsToObserve.toString().split(",");
 
         for (String coin : coinsToFind) {
             coin = coin.replace("[", "").replace("]", "");
 
             if (!coin.isEmpty()) {
-                Log.e("COINTOFIND-SYM", coin);
-
                 Coin coinToAdd = getCoinBySymbol(coin);
                 assert coinToAdd != null;
-                Log.e("COINTOADD", coinToAdd.toString());
                 coinsToShow.add(coinToAdd);
-
             }
         }
     }
@@ -199,7 +159,6 @@ public class WidgetRemoteViewsFactory implements RemoteViewsService.RemoteViewsF
         Type listType = new TypeToken<ArrayList<Coin>>() {
         }.getType();
         coins = (new Gson()).fromJson(serialCoins, listType);
-        Log.e("LOAD_COINS", coins.toString());
     }
 
     private void restorePreferences(Context context) {
@@ -209,9 +168,7 @@ public class WidgetRemoteViewsFactory implements RemoteViewsService.RemoteViewsF
         if (!storedPreferences.isEmpty()) {
 
             String[] parts = storedPreferences.split(" ,");
-
             coinsToObserve.addAll(Arrays.asList(parts));
-            Log.e("PREFERENCES-Restored", coinsToObserve.toString());
         }
     }
 }
